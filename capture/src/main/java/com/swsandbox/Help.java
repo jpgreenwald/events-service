4ffa6ff0743be526a2cb7d302cfc78fc7cbf8f02
Jesse Greenwald <jesse@greenwald>
2013-07-13 14:42:16 -0500

    removed old capture service

---
+++ capture/src/main/java/com/swsandbox/Help.java
@@ -0,0 +1,15 @@
+package com.swsandbox;
+
+/**
+ * User: jgreenwald
+ * Date: 7/13/13
+ * Time: 2:40 PM
+ */
+public class Help
+{
+    public static void main(String[] args)
+    {
+        System.out.printf("");
+
+    }
+}
